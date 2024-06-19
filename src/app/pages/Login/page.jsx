f1b027741d8345de14e6d53ef623b5e7336addbc
woker21 <hernandezrichard674@gmail.com>
2024-06-19 09:24:19 +0200

    sadasds

--- src/app/pages/Login/page.jsx
+++ src/app/pages/Login/page.jsx
@@ -1,9 +1,10 @@
-import { useUserContext } from '../../components/UserProvider';
+'use client'
 import { useEffect, useState } from 'react';
-import { createHilo, signIn, loginWithGoogle, signUp, getUserById } from '../../app/services/users';
+import { useRouter } from 'next/router';
+import { useUserContext } from '../components/UserProvider';
+import { createHilo, signIn, loginWithGoogle, signUp, getUserById } from '../services/users';
 import { onAuthStateChanged, signOut } from 'firebase/auth';
-import { auth } from '../../app/services/api';
-import { useNavigate } from 'react-router-dom';
+import { auth } from '../services/api';
 
 const Home = () => {
     const [user, setUser] = useUserContext();
@@ -11,17 +12,7 @@ const Home = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [account, setCreateAccount] = useState(false);
-
-    /* const login = (e) => {
-        e.preventDefault();
-        if (name.trim() === '') {
-            alert('Introduce un nombre');
-            return;
-        }
-        access(name).then(id => {
-            setUser({ id, name });
-        });
-    } */
+    const router = useRouter();
 
     const register = () => {
         if (name.trim() === '' || email.trim() === '' || password.trim() === '') {
@@ -29,15 +20,12 @@ const Home = () => {
             return;
         }
         signUp(name, email, password).then(id => {
-            /* if (id.includes('Firebase: ')) {
-                alert(id);
-                return;
-            } */
             setUser({ id, name, email });
         }).catch(err => {
             alert(err);
         });
-    }
+    };
+
     const loginWithUserAndPassword = (e) => {
         e.preventDefault();
         if (email.trim() === '' || password.trim() === '') {
@@ -46,32 +34,24 @@ const Home = () => {
         }
 
         signIn(email, password).then(id => {
-            /* if (id.includes('Firebase: ')) {
-                alert(id);
-                return;
-            } */
             getUserById(id).then(user => {
                 setUser({ id, name: user.name, email });
             });
         }).catch(err => {
             alert(err);
         });
-    }
+    };
 
     const googleLogin = () => {
         loginWithGoogle().then(user => {
-            console.log('Google user', user);
-            //setUser({ id: user.uid, name: user.displayName, email: user.email })
+            setUser({ id: user.uid, name: user.displayName, email: user.email });
         });
-    }
-
-    const navigate = useNavigate();
+    };
 
     useEffect(() => {
         if (!user) {
             onAuthStateChanged(auth, user => {
                 if (user) {
-                    console.log(user);
                     if (!user.displayName) {
                         getUserById(user.uid).then(u => {
                             setUser({ id: u.id, name: u.name, email: user.email });
@@ -80,20 +60,16 @@ const Home = () => {
                         setUser({ id: user.uid, name: user.displayName, email: user.email });
                     }
                 } else {
-                    console.log("No user logged");
                     setUser(null);
                 }
             });
         }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+    }, [user, setUser]);
 
     return (
         <div>
             <h1>Home</h1>
-            {/* <button type='submit' onClick={e => login(e)}>Acceder</button> */}
-            <br />
-            {!user ?
+            {!user ? (
                 <form action="">
                     <div>
                         {account ? <h2>Register</h2> : <h2>Login</h2>}
@@ -101,44 +77,45 @@ const Home = () => {
                         <input id="email" type="email" onChange={e => setEmail(e.target.value)} required />
                         <label htmlFor="password">Password</label>
                         <input id="password" type="password" onChange={e => setPassword(e.target.value)} required />
-                        {account &&
+                        {account && (
                             <div>
                                 <label htmlFor="username">Username</label>
-                                <input type="text" id="username" onChange={(e) => setName(e.target.value)} />
+                                <input type="text" id="username" onChange={e => setName(e.target.value)} />
                             </div>
-                        }
+                        )}
                         <br />
                         <div>
-                            {!account ?
+                            {!account ? (
                                 <div>
                                     <br />
-                                    <button type='submit' style={{ float: "left", marginRight: "30px" }} onClick={e => loginWithUserAndPassword(e)}>Log In</button>
-                                    <button type='button' style={{ backgroundColor: "unset", padding: "unset" }} onClick={googleLogin}><img src='/google.PNG' width="40px"></img></button>
+                                    <button type="submit" style={{ float: "left", marginRight: "30px" }} onClick={e => loginWithUserAndPassword(e)}>Log In</button>
+                                    <button type="button" style={{ backgroundColor: "unset", padding: "unset" }} onClick={googleLogin}><img src='/google.PNG' width="40px" alt="Google Login" /></button>
                                     <br />
                                     <br />
                                     <a href="#" onClick={() => setCreateAccount(true)}>Don&apos;t have an account?</a>
                                     <br />
                                     <br />
                                 </div>
-                                :
+                            ) : (
                                 <div>
-                                    <button type='button' onClick={register}>Sign Up</button>
+                                    <button type="button" onClick={register}>Sign Up</button>
                                     <br />
                                     <br />
                                     <a href="#" onClick={() => setCreateAccount(false)}>Already have an account?</a>
                                 </div>
-                            }
+                            )}
                         </div>
                     </div>
                 </form>
-                : <div>
+            ) : (
+                <div>
                     <p>{user.id} - {user.name} - {user.email}</p>
                     <br />
-                    <button type='button' onClick={async () => { await signOut(auth); setUser(null) }}>Logout</button>
+                    <button type="button" onClick={async () => { await signOut(auth); setUser(null); router.push('/'); }}>Logout</button>
                 </div>
-            }
-        </div >
-    )
-}
+            )}
+        </div>
+    );
+};
 
-export default Home
+export default Home;
